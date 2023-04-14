4ff65096c414b8c6926eefc8ac31043efa0cf5c9
anhtv <vietanhtran2069@gmail.com>
2023-04-14 10:59:06 +0700

    Create model connnect to db

---
+++ models/users.js
@@ -0,0 +1,45 @@
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('users', {
+    id: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    email: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    password: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    phone_number: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    date_of_birth: {
+      type: DataTypes.DATEONLY,
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    tableName: 'users',
+    timestamps: false,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "id" },
+        ]
+      },
+    ]
+  });
+};
