9cbdf723f7089d694a9bf8f9a88453a37b65bbd2
NDHoangAnh <hoanganh.axe.no.1@gmail.com>
2023-05-18 23:16:08 +0700

    CRUD voucher

--- routes/admin.js
+++ routes/admin.js
@@ -14,4 +14,10 @@ router.get("/get-service", adminController.getService);
 router.delete("/delete-service", adminController.deleteService);
 router.put("/edit-service", adminController.editService);
 
+// voucher tour
+router.post("/add-voucher", adminController.addVoucher);
+router.get("/get-voucher", adminController.getVoucher);
+router.delete("/delete-voucher", adminController.deleteVoucher);
+router.put("/edit-voucher", adminController.editVoucher);
+
 module.exports = router;
